8a6ca00c97c3ea4f06074800c34e89bd36087181
Aaron Crow <dirtyvagabond@yahoo.com>
2011-07-07 20:55:45 -0700

    Upgraded history fns to canonicalize

--- src/eliza/core.clj
+++ src/eliza/core.clj
@@ -82,8 +82,15 @@
       (println (:output (chat {:input input})))
       (recur))))
 
+(defn canon-str [s]
+  (str/replace (str/upper-case s) #"[^A-Z]" ""))
+
+(defn canon-match? [s1 s2]
+  (= (canon-str s1) (canon-str s2)))
+
 (defn user-has-said? [s]
   (some #(= (first %) s) @*history*))
 
 (defn eliza-has-said? [s]
-  (some #(= (second %) s) @*history*))
+  (some #(canon-match? s (second %)) @*history*))
+
