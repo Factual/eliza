2404458562057c75c8b6f02c2908783bd21b83a3
Evan Gamble <solar.fuel@gmail.com>
2011-07-09 02:15:19 -0700

    added default-respnder

--- src/eliza/core.clj
+++ src/eliza/core.clj
@@ -4,19 +4,38 @@
             (eliza.responders simple
                               delegator)))
 
-;; pairs of entry/response
+;; pairs of entry/response maps
 (def *history* (atom []))
 
+(def default-responder
+  (let [question-responses
+        ["Why do you ask?"
+         "I have no idea."
+         "I wish I knew."
+         "I haven't got a clue."
+         ]
+        statement-responses
+        ["How does that make you feel?"
+         "What would your mother say about that?"
+         "Are you sure?"
+         ]]
+    (fn [{input :input}]
+      {:output (rand-nth (if (= \? (last input))
+                           question-responses
+                           statement-responses))})))
+
 (defn chat [input-map]
-  (let [response-map (some #(% input-map) (vals @*all-responders*))]
-    (swap! *history* conj [input-map response-map])
-    response-map))
+  (let [output-map (or (some #(% input-map) (vals @*all-responders*))
+                       (default-responder input-map))]
+    (swap! *history* conj [input-map output-map])
+    output-map))
 
 (defn chat-loop []
   (loop []
     (print "eliza> ")
+    (flush)
     (when-let [input (not-empty (read-line))]
-      (println (:output (chat {:input input})))
+      (println (:output (chat {:input (str/trim input)})))
       (recur))))
 
 (defn canon-str [s]
--- src/eliza/responders/simple.clj
+++ src/eliza/responders/simple.clj
@@ -2,19 +2,6 @@
   (:use eliza.register)
   (:require [clojure.string :as str]))
 
-(def *simple-question-responses*
-  ["Why do you ask?"
-   "I have no idea."
-   "I wish I knew."
-   "I haven't got a clue."
-   ])
-
-(def *simple-default-responses*
-  ["How does that make you feel?"
-   "What would your mother say about that?"
-   "Are you sure?"
-   ])
-
 (def *swap-me-you-pairs*
   [["me" "y%u"]
    ["I" "y%u"]
@@ -29,26 +16,16 @@
           line *swap-me-you-pairs*))
 
 (defn reflect-input [line]
-  (let [match (re-find #"(.*)([!.?])$" line)
+  (let [match (re-find #"(.*)([!.?])?$" line)
         body (swap-me-you
               (str/trim
                (match 1)))
         response-word (if (= "?" (match 2)) "ask" "say")]
     (str "Why do you " response-word " '" body "'?")))
 
-(defn simple-responder [map]
-  (let [line (:input map)
-        response (cond (and (re-find #"[?]$" line)
-                   (< 0.5 (rand)))
-              (rand-nth *simple-question-responses*)
-
-              (and (re-find #"I|me|you" line)
-                   (< 0.5 (rand)))
-              (reflect-input line)
-              
-              :default
-              (rand-nth *simple-default-responses*)
-              )]
-    {:output response}))
+(defn simple-responder [{input :input}]
+  (when (and (re-find #"I|me|you" input)
+             (< 0.5 (rand)))
+    {:output (reflect-input input)}))
 
 (register-responder ::simple simple-responder)
