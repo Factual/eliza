9ddaf07cb87f0be902dbfdb5c9a756070c146ec2
Will Morgan <wmorgan@factual.com>
2014-05-30 15:00:32 -0700

    Update to latest Clojure

--- project.clj
+++ project.clj
@@ -2,9 +2,4 @@
   :description "Collaborative chatterbot"
   :jar-exclusions [#"\.git"] ; not necessary in a future release of leiningen
   :main eliza.core
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [funnyplaces "1.0-alpha-SNAPSHOT"]]
-  :dev-dependencies [
-	[swank-clojure "1.4.0-SNAPSHOT"]]
-  )
+  :dependencies [[org.clojure/clojure "1.6.0"]])
