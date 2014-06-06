fb913850e943fbd4b56c997e88687b7ca6a9bd76
Will Morgan <wmorgan@factual.com>
2014-06-05 21:18:55 -0700

    Refactor

--- src/eliza/register.clj
+++ src/eliza/register.clj
@@ -4,9 +4,10 @@
 
 (defn wrap-confidence-responder [confidence-fn response-fn]
   (fn [input-map]
-    {:confidence (confidence-fn input-map)
-     :response-ref (future (response-fn input-map))}))
+    {:response-ref (future (response-fn input-map))
+     :confidence   (confidence-fn input-map)}))
 
 (defn register-responder!
   [k confidence-fn response-fn]
-  (swap! responders #(assoc % k (wrap-confidence-responder confidence-fn response-fn))))
+  (let [responder (wrap-confidence-responder confidence-fn response-fn)]
+    (swap! responders #(assoc % k responder))))
