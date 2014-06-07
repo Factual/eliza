eef1acaf23e9ff82707dd13a5387b2de4a7c1c52
Will Morgan <wmorgan@factual.com>
2014-06-06 19:45:47 -0700

    Remove unused code

--- src/eliza/core.clj
+++ src/eliza/core.clj
@@ -9,28 +9,6 @@
             [eliza.middleware :refer [tokenize is-question?]]
             [clojure.string :as str]))
 
-(defn default-responder [input-map]
-  (let [question-responses  ["I have no idea."
-                             "I wish I knew."
-                             "I haven't got a clue."]
-        statement-responses ["I don't know much about that."
-                             "I'm no expert on that."
-                             "I don't have much to say about that."]]
-    {:output (rand-nth (if (:question? input-map)
-                         question-responses
-                         statement-responses))}))
-
-(defn wrap-responder [default-responder other-responder]
-  (fn [input-map]
-    (if-let [responder-response (other-responder input-map)]
-      responder-response
-      (default-responder input-map))))
-
-(defn wrap-confidence-responder [confidence-fn response-fn]
-  (fn [input-map]
-    {:confidence (confidence-fn input-map)
-     :response-ref (future (response-fn input-map))}))
-
 (defn analyzers [m]
   (-> m
       tokenize
